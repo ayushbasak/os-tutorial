32eddf3daa2b91e32b3c38491022297c13a88b63
Ayush Basak <ayushbasak0210@gmail.com>
2021-04-24 00:21:16 -0700

    ADD BASIC PRINTING USING INTERRUPT

--- bootSector.asm
+++ bootSector.asm
@@ -1,5 +1,17 @@
-loop:
-	jmp loop
+mov ah, 0x0e ; enter TTY mode
+mov al, 'A'
+int 0x10
+mov al, 'Y'
+int 0x10
+mov al, 'U'
+int 0x10
+mov al, 'S'
+int 0x10
+mov al, 'H'
+int 0x10
 
-times 51 - ($ - $$) db 0
-dw 0xaa55
+loop:
+    jmp loop 
+
+times 510-($-$$) db 0
+dw 0xaa55 
